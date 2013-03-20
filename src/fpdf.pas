a91d83a3f3fb4f42201ed427612220224a9d9c60
Marcos Douglas <mdbs99@delfire.net>
2013-03-20 11:30:27 -0300

    Updated

--- src/fpdf.pas
+++ src/fpdf.pas
@@ -13,63 +13,62 @@ const
 type
   TFPDF = class
   private
-    page;               // current page number
-    n;                  // current object number
-    offsets;            // array of object offsets
-    buffer;             // buffer holding in-memory PDF
-    pages;              // array containing pages
-    state;              // current document state
-    compress;           // compression flag
-    k;                  // scale factor (number of points in user unit)
-    DefOrientation;     // default orientation
-    CurOrientation;     // current orientation
-    StdPageSizes;       // standard page sizes
-    DefPageSize;        // default page size
-    CurPageSize;        // current page size
-    PageSizes;          // used for pages with non default sizes or orientations
-    wPt, $hPt;          // dimensions of current page in points
-    w, $h;              // dimensions of current page in user unit
-    lMargin;            // left margin
-    tMargin;            // top margin
-    rMargin;            // right margin
-    bMargin;            // page break margin
-    cMargin;            // cell margin
-    x, $y;              // current position in user unit
-    lasth;              // height of last printed cell
-    LineWidth;          // line width in user unit
-    fontpath;           // path containing fonts
-    CoreFonts;          // array of core font names
-    fonts;              // array of used fonts
-    FontFiles;          // array of font files
-    diffs;              // array of encoding differences
-    FontFamily;         // current font family
-    FontStyle;          // current font style
-    underline;          // underlining flag
-    CurrentFont;        // current font info
-    FontSizePt;         // current font size in points
-    FontSize;           // current font size in user unit
-    DrawColor;          // commands for drawing color
-    FillColor;          // commands for filling color
-    TextColor;          // commands for text color
-    ColorFlag;          // indicates whether fill and text colors are different
-    ws;                 // word spacing
-    images;             // array of used images
-    PageLinks;          // array of links in pages
-    links;              // array of internal links
-    AutoPageBreak;      // automatic page breaking
-    PageBreakTrigger;   // threshold used to trigger page breaks
-    InHeader;           // flag set when processing header
-    InFooter;           // flag set when processing footer
-    ZoomMode;           // zoom display mode
-    LayoutMode;         // layout display mode
-    title;              // title
-    subject;            // subject
-    author;             // author
-    keywords;           // keywords
-    creator;            // creator
-    AliasNbPages;       // alias for total number of pages
-    PDFVersion;         // PDF version number
-
+    Fpage: Integer;               // current page number
+    Fn: Integer;                  // current object number
+    Foffsets: array of Integer;   // array of object offsets
+    Fbuffer: TMemoryStream;       // buffer holding in-memory PDF
+    Fpages: array of string;      // array containing pages
+    Fstate: Integer;              // current document state
+    Fcompress: Boolean;           // compression flag
+    Fk: Double;                  // scale factor (number of points in user unit)
+    FDefOrientation: Char;       // default orientation
+    FCurOrientation: Char;       // current orientation
+//    FStdPageSizes;       // standard page sizes
+//    FDefPageSize;        // default page size
+//    FCurPageSize;        // current page size
+//    FPageSizes;          // used for pages with non default sizes or orientations
+    FwPt, FhPt: Double;          // dimensions of current page in points
+    Fw, Fh: Double;              // dimensions of current page in user unit
+    FlMargin: Double;            // left margin
+    FtMargin: Double;            // top margin
+    FrMargin: Double;            // right margin
+    FbMargin: Double;            // page break margin
+    FcMargin: Double;            // cell margin
+    Fx, Fy: Double;              // current position in user unit
+    Flasth: Double;              // height of last printed cell
+    FLineWidth: Double;          // line width in user unit
+    Ffontpath: Double;           // path containing fonts
+    FCoreFonts: array of string;          // array of core font names
+    Ffonts: array of string;              // array of used fonts
+//    FFontFiles: array of Integer;          // array of font files
+    Fdiffs: array of Integer;              // array of encoding differences
+    FFontFamily: string;         // current font family
+    FFontStyle: string;          // current font style
+    Funderline: Boolean;          // underlining flag
+//    FCurrentFont;        // current font info
+    FFontSizePt: Integer;         // current font size in points
+    FFontSize: Integer;           // current font size in user unit
+    FDrawColor;          // commands for drawing color
+    FFillColor;          // commands for filling color
+    FTextColor;          // commands for text color
+    FColorFlag;          // indicates whether fill and text colors are different
+    Fws;                 // word spacing
+    Fimages;             // array of used images
+    FPageLinks;          // array of links in pages
+    Flinks;              // array of internal links
+    FAutoPageBreak;      // automatic page breaking
+    FPageBreakTrigger;   // threshold used to trigger page breaks
+    FInHeader;           // flag set when processing header
+    FInFooter;           // flag set when processing footer
+    FZoomMode;           // zoom display mode
+    FLayoutMode;         // layout display mode
+    Ftitle;              // title
+    Fsubject;            // subject
+    Fauthor;             // author
+    Fkeywords;           // keywords
+    Fcreator;            // creator
+    FAliasNbPages;       // alias for total number of pages
+    FPDFVersion;         // PDF version number
   end;
 
 implementation
